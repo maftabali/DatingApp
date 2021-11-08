5b40444815694562cb45ee8f1e5b8fcd0e4ba0db
Mohammad Aftab <maftabali@yahoo.com>
2021-11-08 21:25:29 +0000

    Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,7 +28,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
-
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -48,6 +48,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
